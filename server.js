df96d8640b5f68c30cd63ee334402e50dd5e027c
paperpatch <patchen21@gmail.com>
2021-11-28 15:03:43 -0500

    12.2

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'sql21',
+    password: '',
     database: 'election'
   },
-  console.log('Connected to the election database')
+  console.log('Connected to the election database.')
 );
 
 // Get all candidates
@@ -38,7 +38,6 @@ app.get('/api/candidates', (req, res) => {
   });
 });
 
-
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
   const sql = `SELECT * FROM candidates WHERE id = ?`;
@@ -55,8 +54,6 @@ app.get('/api/candidate/:id', (req, res) => {
     });
   });
 });
-
-
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
@@ -82,13 +79,19 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
+
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-    VALUES (?, ?, ?)`;
+    VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
 
   db.query(sql, params, (err, result) => {
